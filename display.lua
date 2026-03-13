472409c20c904c044e7619b3914a3a13cb324b42
dvh <vinnyhorgan@gmail.com>
2026-03-13 16:12:16 +0100

    fix min size

--- display.lua
+++ display.lua
@@ -3,10 +3,10 @@ local Display = {}
 Display.virtual_w = 1440
 Display.virtual_h = 800
 
-Display.window_w = Display.virtual_w
-Display.window_h = Display.virtual_h
-
-Display.min_window_h = 500
-Display.min_window_w = math.floor(Display.min_window_h * (Display.virtual_w / Display.virtual_h) + 0.5)
+Display.min_scale = 0.85
+Display.min_window_w = math.floor(Display.virtual_w * Display.min_scale + 0.5)
+Display.min_window_h = math.floor(Display.virtual_h * Display.min_scale + 0.5)
+Display.window_w = Display.min_window_w
+Display.window_h = Display.min_window_h
 
 return Display
